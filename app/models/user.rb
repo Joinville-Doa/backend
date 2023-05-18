5c3ce174c8453c4d91ac01e8b9f033ed2bf66f0d
kanamydamiao <kanamystewart@gmail.com>
2023-05-17 21:15:41 -0300

    Adicionar controllers de usuários

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :name, :email, :document_number, :phone, :accept_terms_of_use, presence: true
 end
