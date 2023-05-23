2c2fe6325f512c3f0bffcfa2d016fb1a20f0c19d
kanamydamiao <kanamystewart@gmail.com>
2023-05-22 21:34:14 -0300

    Ajustes no get de todos usuários, e teste do model

--- Gemfile
+++ Gemfile
@@ -20,7 +20,6 @@ gem "devise"
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem "rspec-rails"
-  gem "factory_bot_rails"
   gem "faker"
   gem 'simplecov', require: false
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,21 @@
 module Types
   class QueryType < Types::BaseObject
+    field :users, [Types::UserType], null: false
     field :user, Types::UserType, null: true do
       description 'Find a user by ID'
       argument :id, ID, required: true
-      argument :email, String, required: false
     end
-
-    def user(id:)
-      User.find(id)
+    field :user_by_email, Types::UserType, null: true do
+      description 'Find a user by email'
+      argument :email, String, required: true
     end
 
     def users
       User.all
+    end
+
+    def user(id:)
+      User.find(id)
     end
 
     def user_by_email(email:)
