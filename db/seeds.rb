6c3d32360e6cfe23645eed5151573cd80cc3e90f
kanamydamiao <kanamystewart@gmail.com>
2023-06-26 16:26:57 -0300

    Ajustar banco

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,24 @@
 require 'faker'
 
 # Create 100 fake users
-10.times do
-  password =  Faker::Internet.password(min_length: 6)
-  User.create!(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    document_number: Faker::IDNumber.valid,
-    date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
-    phone: Faker::PhoneNumber.phone_number,
-    password: password,
-    password_confirmation: password,
-    accept_terms_of_use: true
-  )
+# 10.times do
+#   password =  Faker::Internet.password(min_length: 6)
+#   User.create!(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.email,
+#     document_number: Faker::IDNumber.valid,
+#     date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
+#     phone: Faker::PhoneNumber.phone_number,
+#     password: password,
+#     password_confirmation: password,
+#     accept_terms_of_use: true
+#   )
 
-  puts "User #{User.last.name} created"
-end
+#   puts "User #{User.last.name} created"
+# end
 
 #Create categories
 categories = [
-  "Alimentos",
   "Roupas",
   "Calçados",
   "Móveis",
@@ -29,8 +28,7 @@ categories = [
   "Livros",
   "Ferramentas",
   "Itens para pets",
-  "Peças Automotivas",
-  "Outros"
+  "Peças Automotivas"
 ]
 categories.each do |category|
   Category.create!(
@@ -41,21 +39,21 @@ categories.each do |category|
 end
 
 # Create 100 donations with random user
-100.times do
-  Donation.create!(
-    user: User.all.sample,
-    title: Faker::Lorem.sentence,
-    description: Faker::Lorem.paragraph,
-    phone_contact: Faker::PhoneNumber.phone_number,
-    new_product: Faker::Boolean.boolean,
-    has_whatsapp: Faker::Boolean.boolean,
-    category_id: Category.all.sample.id,
-    image_one: Faker::Avatar.image,
-    image_two: Faker::Avatar.image,
-    image_three: Faker::Avatar.image,
-    image_four: Faker::Avatar.image,
-    image_five: Faker::Avatar.image
-  )
+# 100.times do
+#   Donation.create!(
+#     user: User.all.sample,
+#     title: Faker::Lorem.sentence,
+#     description: Faker::Lorem.paragraph,
+#     phone_contact: Faker::PhoneNumber.phone_number,
+#     new_product: Faker::Boolean.boolean,
+#     has_whatsapp: Faker::Boolean.boolean,
+#     category_id: Category.all.sample.id,
+#     image_one: Faker::Avatar.image,
+#     image_two: Faker::Avatar.image,
+#     image_three: Faker::Avatar.image,
+#     image_four: Faker::Avatar.image,
+#     image_five: Faker::Avatar.image
+#   )
 
-  puts "Donation #{Donation.last.title} created"
-end
+#   puts "Donation #{Donation.last.title} created"
+# end
